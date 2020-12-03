36c7c67eb8433eedad12498a79627dfc2b2b3844
Talia Gonzalez <taliagonzalez.dla@gmail.com>
2020-12-02 23:50:06 -0600

    Crear un arreglo

--- src/cipher.js
+++ src/cipher.js
@@ -3,3 +3,9 @@ const cipher = {
 };
 
 export default cipher;
+
+const alphabet = ['A','B','C','D','E','F','G','H','I','J','K',
+                  'L','M','N','O','P','Q','R','S','T','U','V',
+                  'W','X','Y','Z']
+
+                  
