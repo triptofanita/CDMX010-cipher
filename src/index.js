696454039d637f5a048267c1aa68918370da40d4
Talia Gonzalez <taliagonzalez.dla@gmail.com>
2020-12-04 00:10:52 -0600

    Correponde a maquetación HTML

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+
+document.elementById('name-refugee').addEventeListener (keyup,function (){ //ésta función permite que lo que escribe el usuario se convierta en mayúsculas
+ //mantener el texto en mayúsculas 
+});
+
+
+
+
+
+
+
 import cipher from './cipher.js';
 
 console.log(cipher);
-
