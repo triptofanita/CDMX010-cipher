079b2dd9b76aa8f338badb403b33f3cba47ff031
Talia Gonzalez <taliagonzalez.dla@gmail.com>
2020-12-04 23:57:09 -0600

    Elementos del DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
-document.elementById('name-refugee').addEventeListener (keyup,function (){ //ésta función permite que lo que escribe el usuario se convierta en mayúsculas
- //mantener el texto en mayúsculas 
+
+
+document.getElementById('code').addEventListener('click',function(){ //Se agrega un evento al botón id 'code'
+    let value = document.getElementById('name-refugee').value.toUpperCase(); //declaro la variable value, agrego al id 'name-refugge' que su contenido esté em mayúsculas
+    document.getElementById('result-refugee').textContent = value; //en 'result-refugee' obtener el texto contenido del value, o sea de la línea anterior
 });
 
 
@@ -9,6 +12,4 @@ document.elementById('name-refugee').addEventeListener (keyup,function (){ //és
 
 
 
-import cipher from './cipher.js';
 
-console.log(cipher);
