b489873b33644ce3990119204b0ac17b457b9c0d
Talia Gonzalez <taliagonzalez.dla@gmail.com>
2020-12-18 01:27:07 -0600

    Módulo listo

--- src/cipher.js
+++ src/cipher.js
@@ -1,31 +1,30 @@
 const cipher = {
   
-  encode (string,offset)  {
-    let output =[];
-    //offset = parseInt(offset,10);
+  encodeString(string, offset)  {
+    let output = [];
+    offset = parseInt(offset, 10);
 
-    for (let i = 0 ; i < string.length; i ++) {
-    let asciiLetter = string.charCodeAt(i);
-    let stringResult = ((asciiLetter + 65 - offset) % 26 + 65);
-     output[i] = String.fromCharCode(stringResult);
-    };
+    for (let i = 0; i < string.length; i++) {
+      let asciiLetter = string.charCodeAt(i);
+      let stringResult = ((asciiLetter - 65 + offset) % 26 + 65);
+      output[i] = String.fromCharCode(stringResult);
+    }
     return output;
     //document.getElementById('result-refugee').textContent = output.join("");
-  };
+  },
 
-
- decode (string,offset) {
-    let output =[];
-    for (let i = 0 ; i < string.length; i ++) {
+ decodeString(string, offset) {
+    let output = [];
+    for (let i = 0; i < string.length; i++) {
      let asciiLetter = string.charCodeAt(i);
      let stringResult = ((asciiLetter + 65 - offset) % 26 + 65);
      output[i] = String.fromCharCode(stringResult);
-    };
+    }
     return output;
     //document.getElementById('result-refugee').textContent = output.join("");
   }
 };
 
-export default '/cipher.js';
+export default cipher;
 
   
--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 import cipher from './cipher.js'
 
-
-const string = document.getElementById('name-refugee')
-//const stringResult = document.getElementById('clave-refugee')
 const buttonEncode = document.getElementById('encode')
 const buttonDecode = document.getElementById('decode')
-//const stringResult = document.getElementById('result-refugee').toUpperCase();
-const offset = Number.parseInt(document.getElementById('offset'))
+
 let output = [];
 
-buttonEncode.addEventListener('click', encode);
-function encode () {
-    document.getElementById('name-refugee').value;
-    output = cipher.enconde(string,offset);
-    document.getElementById('result-refugee').value.toUpperCase();
-    document.getElementById('result-refugee').textContent = output.join('');
-};
- 
-buttonDecode.addEventListener('click', decode);
-function decode () {
-    document.getElementById('clave-refugee').value;
-    output = cipher.decode(string,offset);
-    document.getElementById('result-refugee').value.toUpperCase();
-    document.getElementById('result-refugee').textContent = output.join('');
-};
+buttonEncode.addEventListener('click', encodeListener);
+    function encodeListener(){
+        const stringName = (document.getElementById('name-refugee').value).toUpperCase();
+        const offset = Number.parseInt(document.getElementById('offset').value)
+        output = cipher.encodeString(stringName, offset);
+        document.getElementById('result-refugee').textContent = output.join('');
+    }
 
-window.encode = encode
-window.decode = decode
+buttonDecode.addEventListener('click', decodeListener);
+    function decodeListener(){
+        const stringResult = (document.getElementById('clave-refugee').value).toUpperCase();
+        const offset = Number.parseInt(document.getElementById('offset').value)
+        output = cipher.decodeString(stringResult, offset);
+        document.getElementById('result-refugee').textContent = output.join('');
+    }
+
